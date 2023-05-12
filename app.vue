aecdcfa3c82a315c0632d38c061f9b99343d8115
Isidorev215 <nwaiwuisidore@gmail.com>
2023-05-12 12:06:37 -0600

    Added some SEO code nuxt_schema.org nuxt_sitemap meta tags like og and twitter:card in nuxt.config

--- app.vue
+++ app.vue
@@ -3,13 +3,13 @@
     <Navbar />
 
     <main class="flex justify-center lg:grid lg:grid-cols-3 h-[calc(100vh-9rem)]">
-      <section class="hidden lg:col-span-2 lg:flex justify-center items-center px-4 pt-5 pb-1">
+      <section class="items-center justify-center hidden px-4 pt-5 pb-1 lg:col-span-2 lg:flex">
         <div class="">
-          <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Every man has to own a Black Suit.</h1>
+          <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Every man has to own a Black Suit.</h1>
           <p class="max-w-2xl font-light text-gray-500 md:text-lg lg:text-xl dark:text-gray-400">
             <span>There are five suits you need to get in all. You should not mess it up.... It's all on you.... Make your 70+ suit combinations.</span>
           </p>
-          <p class="hidden lg:block mt-3 mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p class="hidden max-w-2xl mt-3 mb-6 font-light text-gray-500 lg:block lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             <button @click="changeFullSuit(suitColors.BLACK)" class="bg-stv-black pill">Black </button>
             <button @click="changeFullSuit(suitColors.BROWN)" class="bg-stv-brown pill">Brown </button>
             <button @click="changeFullSuit(suitColors.GRAY)" class="bg-stv-gray pill">Gray </button>
@@ -17,14 +17,14 @@
             <button @click="changeFullSuit(suitColors.TAN)" class="bg-stv-tan pill">Tan </button>
           </p>
         
-          <button @click="generateRandom" title="So sayeth Steve Harvey" class="inline-flex justify-center items-center py-3 px-5 mt-2 lg:mt-0 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+          <button @click="generateRandom" title="So sayeth Steve Harvey" class="inline-flex items-center justify-center px-5 py-3 mt-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg lg:mt-0 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
             Randomize
           </button>
         </div>
       </section>
-      <section class="w-96 flex flex-col lg:col-span-1 justify-center items-center bg-white dark:bg-gray-900">
-        <div class="lg:hidden top w-full h-full flex justify-center items-center">
-          <h2 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-3xl xl:text-4xl dark:text-white">So Sayeth Steve!</h2>
+      <section class="flex flex-col items-center justify-center bg-white w-96 lg:col-span-1 dark:bg-gray-900">
+        <div class="flex items-center justify-center w-full h-full lg:hidden top">
+          <h2 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-3xl xl:text-4xl dark:text-white">So Sayeth Steve!</h2>
         </div>
         <SuitOne class="w-full" :propColor="fullSuitPayload" @emitGenerateRandom="generateRandom" @updateSuitSelectionForwarded="updateSuitSection"/>
       </section>
@@ -40,6 +40,15 @@
 <script setup lang="ts">
 import { suitColors, shirtColors, suitSection } from '~/types/enums'
 import { fullSuitInterface, selectionInterface } from '~/types/interfaces'
+
+  useSchemaOrg([
+    // Using only website as schema.org identity for now for Jsonld
+    defineWebSite({
+      name: 'Suits by Steve',
+      image: '/assets/img/logo.png'
+    }),
+    defineWebPage(),
+  ])
 
   const fullSuitPayload = ref<fullSuitInterface>({
     jacket: suitColors.BLACK,
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon"],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "nuxt-icon", "nuxt-schema-org", "nuxt-simple-sitemap"],
   app: {
     head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
       title: 'Suits By Steve',
       meta: [
-        { name: 'description', content: 'Every man has to own a black suit | Black, Brown, Gray, Navy, Tan. Visualizing suit recommendation by Steve Harvey. You cannot mess it up' }
-      ]
+        { name: 'description', content: 'Every man has to own a black suit | Black, Brown, Gray, Navy, Tan. Visualizing suit recommendation by Steve Harvey. You cannot mess it up' },
+        { property: 'og:title', content: 'Suits By Steve' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:description', content: 'Every man has to own a black suit | Black, Brown, Gray, Navy, Tan. Visualizing suit recommendation by Steve Harvey. You cannot mess it up' },
+        { property: 'og:image', content: 'https://suitsbysteve.web.app/home_page_image.png' },
+        { property: 'og:url', content: 'https://suitsbysteve.web.app' },
+        { property: 'twitter:card', content: 'https://suitsbysteve.web.app/home_page_image.png' }
+      ],
+      htmlAttrs: { lang: 'en' }
     }
-  }
+  },
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://suitsbysteve.web.app'
+    }
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/']
+    }
+  },
+  schemaOrg: {
+    host: 'https://suitsbysteve.web.app'
+  },
 })
